51b87a5fa6b9f721d78ffb006f2e63afaac4ba7f
Kordasauter <machinbidule2014@gmail.com>
2024-04-18 20:55:43 +0200

    fix routes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,9 +18,9 @@ function Header({ user, setUser }) {
       <div className="container">
         <img src={Logo} alt="logo mpm vieu grimoire" />
         <ul>
-          <li><NavLink to="/" end className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Accueil</NavLink></li>
-          <li><NavLink to="/Ajouter" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Ajouter un livre</NavLink></li>
-          <li>{!user ? <NavLink to="/Connexion" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Se connecter</NavLink> : <span tabIndex={0} role="button" onKeyUp={disconnect} onClick={disconnect}>Se déconnecter</span> }</li>
+          <li><NavLink to="/P7-Dev-Web-livres/" end className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Accueil</NavLink></li>
+          <li><NavLink to="/P7-Dev-Web-livres/Ajouter" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Ajouter un livre</NavLink></li>
+          <li>{!user ? <NavLink to="/P7-Dev-Web-livres/Connexion" className={({ isActive }) => (isActive ? styles.activeLink : undefined)}>Se connecter</NavLink> : <span tabIndex={0} role="button" onKeyUp={disconnect} onClick={disconnect}>Se déconnecter</span> }</li>
         </ul>
       </div>
     </header>
